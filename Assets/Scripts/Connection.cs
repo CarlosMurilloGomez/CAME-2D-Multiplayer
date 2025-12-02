716bde35959d803ab3ca21d7a960ea449b34af91
Carlos <charliemurillo2001@gmail.com>
2025-12-02 20:58:09 +0100

    Añadir gemas y Lobby

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
+using TMPro;
 
 public class Connection : MonoBehaviourPunCallbacks
 {
     public static int IDjugador;
     private bool cone;
-    public Button boton;
+    public Button[] botonesInicio;
+    public Button volverAInicio;
+    public GameObject panelCrearSala;
+    public GameObject panelUnirse;
+    public GameObject panelSalaMaster;
+    public GameObject panelSalaClient;
+
+    public TextMeshProUGUI NombreSala;
+    public Button botonCrearSala;
     void Start()
     {
+        botonesInicio[0].interactable = false;
+        botonesInicio[1].interactable = false;
         cone = false;
         PhotonNetwork.ConnectUsingSettings();
     }
@@ -20,17 +31,24 @@ public class Connection : MonoBehaviourPunCallbacks
     }
     override public void OnConnectedToMaster()
     {
+        botonesInicio[0].interactable = true;
+        botonesInicio[1].interactable = true;
         Debug.Log("Conectado al servidor");
     }
 
-    public void ButtonConnect()
+    public void CrearSala()
     {
-        RoomOptions options = new RoomOptions();
-        options.MaxPlayers = 2;
+        if ( PhotonNetwork.CountOfRooms <= 3)
+        {
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = 2;
 
-        PhotonNetwork.JoinOrCreateRoom("room1", options, TypedLobby.Default);
-        cone = true;
-        boton.interactable = false;
+            PhotonNetwork.JoinOrCreateRoom(NombreSala.text, options, TypedLobby.Default);
+            botonCrearSala.interactable = false;
+            panelCrearSala.SetActive(false);
+            panelSalaMaster.SetActive(true);
+        }
+
     }
 
     override public void OnJoinedRoom()
@@ -40,13 +58,48 @@ public class Connection : MonoBehaviourPunCallbacks
         IDjugador = PhotonNetwork.LocalPlayer.ActorNumber;
         Debug.Log("ID del jugador: " + IDjugador);
 
-       
-
-        
     }
+
+    public void IrACrearSala()
+    {
+        panelCrearSala.SetActive(true);
+    }
+
+    public void VolverAlInicio()
+    {
+        panelCrearSala.SetActive(false);
+        panelUnirse.SetActive(false);
+
+    }
+
+    public void VolverAlInicioDesdeSala()
+    {
+        panelSalaMaster.SetActive(false);
+        if (PhotonNetwork.InRoom)
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        if (PhotonNetwork.LocalPlayer == newMasterClient)
+        {
+            photonView.RPC("RPC_SalirSala", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    public void RPC_SalirSala()
+    {
+        PhotonNetwork.LeaveRoom();
+        panelSalaClient.SetActive(false);
+    }
+
+
 
     private void Update()
     {
+     
         if (cone)
         {
             if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount > 1)
@@ -54,13 +107,11 @@ public class Connection : MonoBehaviourPunCallbacks
                 PhotonNetwork.LoadLevel(1);
                 Destroy(this);
             }
-            else
+            else if (!PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount > 1)
             {
-                if (!PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount > 1)
-                {
-                    PhotonNetwork.LoadLevel(1);
-                    Destroy(this);
-                }
+                PhotonNetwork.LoadLevel(1);
+                Destroy(this);
+                
             }
         }
     }
